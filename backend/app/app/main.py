33a14fbe7c540a50489341541e0c78e766910b42
Anish Dalal <anishdalal@Anishs-MBP.home>
2021-11-29 13:21:49 -0500

    add gpt-3 summarization layer

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -7,6 +7,7 @@ import os
 import re
 
 from fastapi import FastAPI, Request
+import openai
 import pdfminer.high_level
 from slack_bolt import App
 from slack_bolt.adapter.fastapi import SlackRequestHandler
@@ -41,7 +42,7 @@ REGEX_EXP = r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
 search_model = SentenceTransformer('msmarco-distilbert-base-v4')
 db = database.SessionLocal()
 
-
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.event("file_created")
 def handle_file_created_events(event, say):
@@ -277,7 +278,6 @@ def view_more_results(ack, body, client):
     ack()
     query = body["actions"][0]["value"]
     team = body["team"]["id"]
-    logger.info(team)
     embedding = search_model.encode([query])
     documents = crud.get_documents(db, team)
     snippets = _get_k_most_similar_docs(documents, embedding)
@@ -434,6 +434,20 @@ def process_query(event, say, query):
                 snippet = sentences[corpus_id]
             else:
                 snippet = " ".join(sentences[corpus_id-1:corpus_id+2])
+            if team == "T02KCNMCUHE" or team == "T02MGVB1HL5":
+                snippet = " ".join(sentences[corpus_id:corpus_id+2])
+                snippet_processed = " ".join(snippet.split("\n")).strip()
+                response = openai.Completion.create(
+                    engine="curie",
+                    prompt=f"Original: The Company and the Founders will provide the Investors with customary representations and warranties examples of which are set out in Appendix 4 and the Founders will provide the Investors with customary non-competition, non-solicitation and confidentiality undertakings.\nSummary: The Company and its Founders will provide the usual assurances and guarantees on facts about the business. The founders will also agree not to work for competitors, poach employees or customers when they leave the startup, and respect confidentiality.\n###\nOriginal: One immediately obvious and enormous area for Bitcoin-based innovation is international remittance. Every day, hundreds of millions of low-income people go to work in hard jobs in foreign countries to make money to send back to their families in their home countries – over $400 billion in total annually, according to the World Bank.\nSummary: Bitcoin can be an innovation for sending money overseas. The market opportunity is large. Workers send over $400 billion annually to their families in their home countries. \n###\nOriginal: In the event of an initial public offering of the Company's shares on a US stock exchange the Investors shall be entitled to registration rights customary in transactions of this type (including two demand rights and unlimited shelf and piggy-back rights), with the expenses paid by the Company.\nSummary: If the Company does an IPO in the USA, investors have the usual rights to include their shares in the public offering and the costs of d doing this will be covered by the Company.\n###\nOriginal: Finally, a fourth interesting use case is public payments. This idea first came to my attention in a news article a few months ago. A random spectator at a televised sports event held up a placard with a QR code and the text “Send me Bitcoin!” He received $25,000 in Bitcoin in the first 24 hours, all from people he had never met. This was the first time in history that you could see someone holding up a sign, in person or on TV or in a photo, and then send them money with two clicks on your smartphone: take the photo of the QR code on the sign, and click to send the money.\nSummary: Public payments is an interesting use case for Bitcoin. A person collected $25,000 in Bitcoin from strangers after holding up a QR code. It was the first time in history such an event occured.\n###\nOriginal: {snippet_processed}\n",
+                    temperature=0.1,
+                    max_tokens=64,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                    stop=["\n"]
+                )
+                snippet = response["choices"][0]["text"].split("Summary: ")[1]
             blocks = [
                 {
                     "type": "section",
