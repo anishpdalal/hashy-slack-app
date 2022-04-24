2623987ccfd86ed2595825be6e28114fb6e206cc
Anish Dalal <anishdalal@Anishs-MacBook-Pro.local>
2022-04-23 22:26:54 -0400

    ugh

--- core/setup.py
+++ core/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="core",
     version="0.1.0",
-    packages=find_packages(include=["core", "core"]),
+    packages=find_packages(include=["core", "core.*"]),
     python_requires=">=3.8",
     install_requires=[
         "psycopg2-binary",
