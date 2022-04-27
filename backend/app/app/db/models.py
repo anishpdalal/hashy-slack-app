8b4a9969d2dfe27403151815c3c849bd580033c6
Anish Dalal <anishdalal@Anishs-MacBook-Pro.local>
2022-04-26 23:44:06 -0400

    add new models

--- backend/app/app/db/models.py
+++ backend/app/app/db/models.py
@@ -1,7 +1,7 @@
 import os
 import uuid
 
-from sqlalchemy import Column, Integer, PickleType, String, Text, DateTime
+from sqlalchemy import Boolean, Column, Integer, PickleType, String, Text, DateTime
 from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy.sql import func
 from sqlalchemy_utils import EncryptedType
@@ -81,3 +81,48 @@ class GoogleToken(Base):
     time_updated = Column(DateTime(timezone=True), onupdate=func.now())
     last_cursor = Column(String)
  
+
+class SlackUser(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, nullable=False)
+    team_name = Column(String, nullable=False)
+    team_id = Column(String, nullable=False)
+    created = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated = Column(DateTime(timezone=True), onupdate=func.now())
+    onboarded = Column(Boolean, default=False)
+
+
+class ContentStore(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    team_id = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    source_id = Column(String, nullable=False, unique=True)
+    user_ids = Column(ARRAY(String))
+    source_last_updated = Column(DateTime(timezone=True))
+    num_vectors = Column(Integer, default=0)
+    created = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated = Column(DateTime(timezone=True), onupdate=func.now())
+    is_boosted = Column(Boolean, default=False)
+    parent = Column(String)
+
+
+class Integration(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    team_id = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+    token = Column(
+        EncryptedType(String, os.environ["TOKEN_SEC_KEY"],
+        AesEngine,
+        "pkcs5"
+    ), nullable=False)
+    user_id = Column(String)
+    created = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated = Column(DateTime(timezone=True), onupdate=func.now())
+    last_cursor = Column(String)
+    extra = Column(String)
