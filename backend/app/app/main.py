2f4d860193814d63812e5ce6821ece0b1f8e1693
Anish Dalal <anishdalal1@gmail.com>
2022-05-05 22:36:55 -0400

    enforce question length

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -152,7 +152,7 @@ def handle_message_channel(event, say, client):
         user = event.get("user")
         query = event.get("text")
         cleaned_query = re.sub(r'http\S+', '', query) if query else None
-        if query and "?" in cleaned_query and user and team:
+        if query and "?" in cleaned_query and user and team and len(cleaned_query.split()) > 10:
             response = requests.post(
                 f"{os.environ['API_URL']}/search",
                 data=json.dumps(
